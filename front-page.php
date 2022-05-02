2755350730f25a42d4564bb16a1bc4f9ef52655d
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-05-02 15:36:41 -0300

    lot of changes for better responsiveness

--- front-page.php
+++ front-page.php
@@ -1,17 +1,19 @@
 <?php
   get_header();
 ?>
-
-<div class="flex">
-  <div>
-    <h1>Hi, I'am Marlon Web Designer</h1>
-    <a class="btn btn-primary" href="#">Contact</a>
-  </div>
-  <div class="text-decoration-none">
+<div class="container">
+  <div class="d-flex justify-content-between">
+    <div class="d-flex flex-column justify-content-around">
+      <h1>Hi, I'am <span class="text-primary">Marlon</span> Web Designer</h1>
+      <a class="btn btn-primary" href="#">Contact</a>
+    </div>
+    <div class="text-decoration-none">
     <img src="<?php bloginfo("template_directory"); ?>/img/nerd.png" alt="portfolio image">
+    </div>
+  </div>
+  <div class="d-flex justify-content-start">
+    <?php
+      get_footer();
+    ?>
   </div>
 </div>
-
-<?php
-  get_footer();
-?>
