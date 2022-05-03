b719b3b134ca7db866606e862ef41d96c4350043
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-05-03 11:52:00 -0300

    styles and condition that detects if has featured image, if there's not it displays an default image

--- single.php
+++ single.php
@@ -2,10 +2,25 @@
   get_header();
 ?>
 
-<img src="<?php the_post_thumbnail_url(); ?>" alt="">
-<h1><?php the_title(); ?></h1>
-<div><?php the_content(); ?></div>
-
 <?php
-  get_footer();
+  if(has_post_thumbnail()) // Caso tenha featured img no blog ela será mostrada normalmente
+  {
+?> 
+  <img class="img-fluid"  src="<?php the_post_thumbnail_url(); ?>" alt=""> 
+<?php
+  } else {
 ?>
+  <img class="img-fluid"  src="<?php bloginfo("template_directory")?>/img/nerd.png" alt=""> <!-- Caso contrário será mostrada uma imagem default -->
+<?php
+  }
+?>
+
+<h1 class="px-5 pt-2"><?php the_title(); ?></h1>
+<div class="px-5"><?php the_content(); ?></div>
+
+<div class="d-flex flex-column justify-content-center align-items-center w-auto">
+  <p>Minhas mídias sociais</p>
+  <?php
+    get_footer();
+  ?>
+</div>
